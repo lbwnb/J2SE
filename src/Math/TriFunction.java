89f10b598a67438e450d837ab1233ad8a1d72f81
lbwnb <61997011@qq.com>
2018-07-06 23:22:16 +0800

    Math类的常量

---
+++ src/Math/TriFunction.java
@@ -0,0 +1,14 @@
+package Math;
+
+public class TriFunction {
+public static void main(String[] args) {
+	System.out.println("sin30 = " + Math.sin(Math.PI/6));
+	//计算30°的正弦值，参数是弧度表示的角，即Π的六分之一
+	
+	System.out.println("cos30 = " + Math.cos(Math.PI/6));
+	//计算30°的余弦值  计算三角函数的方法，参数和返回值的类型都为double
+	
+	System.out.println("tan30 = " +Math.tan(Math.PI/6));
+	//计算30°的正切值
+}
+}
